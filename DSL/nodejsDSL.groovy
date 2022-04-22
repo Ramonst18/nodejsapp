562fe9dd43f975d7685515efb0ca8526e031e7a7
Ramón López <63748740+Ramonst18@users.noreply.github.com>
2022-04-21 18:15:58 -0700

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/Ramonst18/simple-java-maven-app.git', 'master') { node ->
+              node / gitConfigName('Ramonst18')
+              node / gitConfigEmail('Ramonst18@hotmail.com')
         }
     }
     triggers {
